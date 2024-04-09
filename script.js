0707377f5c73576837a886bb1f086d1602337f7b
kelly <W0433053@nscc.ca>
2024-04-09 19:59:13 -0300

    More fixes

--- script.js
+++ script.js
@@ -3,9 +3,13 @@
 let projectsNodeList = document.querySelectorAll('.project-tile');
 projectsNodeList.forEach( project => {
     let projectDialog = project.parentElement.querySelector('dialog');
-    console.log(projectDialog);
+    let dialogXBtn = projectDialog.querySelector('.close-btn');
     project.addEventListener('click', event => {
         event.preventDefault();
         projectDialog.showModal();
+    })
+
+    dialogXBtn.addEventListener('click', event => {
+        projectDialog.close();
     })
 })
