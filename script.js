7c804588fb01cfe877a01fe0e04b0a2ce63ebb3d
kelly <W0433053@nscc.ca>
2024-04-09 21:24:38 -0300

    Theme Switcher

--- script.js
+++ script.js
@@ -13,3 +13,37 @@ projectsNodeList.forEach( project => {
         projectDialog.close();
     })
 })
+
+let themeSwitcher = document.querySelector('#theme-switcher');
+document.addEventListener('DOMContentLoaded', e => {
+    let bodyTag = document.querySelector('body');
+    let savedTheme = localStorage.getItem('saved-theme');
+    bodyTag.classList = [];
+    
+    if ( savedTheme ) {
+        bodyTag.classList.add(savedTheme);
+    }
+
+    if ( savedTheme === 'dark') {
+        themeSwitcher.selectedIndex = 1;
+    }
+
+    if ( savedTheme === 'light') {
+        themeSwitcher.selectedIndex = 2;
+    }
+
+    if ( !savedTheme ) {
+        themeSwitcher.selectedIndex = 0;
+    }
+})
+
+
+themeSwitcher.addEventListener('change', e => {
+    let selectedTheme = themeSwitcher.value;
+    let bodyTag = document.querySelector('body');
+    bodyTag.classList = [];
+    if ( selectedTheme ) {
+        bodyTag.classList.add(selectedTheme);
+    }
+    localStorage.setItem('saved-theme', selectedTheme);
+})
